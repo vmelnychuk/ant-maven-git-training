de80bdba03538678b8f6b8e0a657260b0f4e4242
Vasyl Melnychuk <vasyl_melnychuk@epam.com>
2014-01-24 17:48:48 +0300

    change pom to right mainclass, add inputfile sample

--- src/main/java/read/App.java
+++ src/main/java/read/App.java
@@ -1,17 +1,28 @@
 package read;
 
-import org.fusesource.jansi.Ansi;
-import org.fusesource.jansi.AnsiConsole;
+import java.io.File;
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-    	AnsiConsole.systemInstall();
-    	System.out.println(
-    			Ansi.ansi().eraseScreen().fg(Ansi.Color.BLUE).a("Hello").fg(Ansi.Color.YELLOW).a(" World").reset()
-    			);
-        //System.out.println(Ansi.ansi().eraseScreen().render("@|red Hello|@ @|yellow World|@") );
-        AnsiConsole.systemUninstall();
+    	if (args.length != 2) {
+    		System.out.println("Error:\n usage 'program inputFile outputFile'");
+    		System.exit(1);
+    	}
+    	final String dir = System.getProperty("user.dir");
+    	String inputFileName = args[0];
+    	String outputFileNanme = args[1];
+    	File inputFile = new File(inputFileName);
+    	File outputFile = new File(outputFileNanme);
+    	
+    	if (inputFile.exists()) {
+    		System.out.println("inputfile: OK" + " out " + outputFile.exists());
+    		System.out.println(inputFile.getAbsolutePath());
+    	} else {
+    		System.out.println("No input file");
+    	}
+        System.out.println("current dir = " + dir);
     }
 }
