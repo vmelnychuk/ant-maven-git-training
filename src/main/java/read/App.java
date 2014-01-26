215eb7639842eeca78205ef7fbc5613e9b445bab
Vasyl Melnychuk <Vasyl_Melnychuk@epam.com>
2014-01-26 22:25:27 +0300

    create reverse output

--- src/main/java/read/App.java
+++ src/main/java/read/App.java
@@ -4,14 +4,12 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.Closeable;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
@@ -69,6 +67,7 @@ public class App {
 				while ((c = in.read()) != -1) {
 					fileStore.add(c);
 				}
+				Collections.reverse(fileStore);
 			} else {
 				System.err.println("problems with input file");
 			}
